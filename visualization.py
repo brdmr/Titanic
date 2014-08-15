d2c94f938bc221c67ebc6993c8c9fbec31404db9
Freddeb <f.bredmar@gmail.com>
2014-08-15 11:09:11 +0200

    Some general cleanup, mostly comments. Extracted some info about passengers gender

--- visualization.py
+++ visualization.py
@@ -16,18 +16,30 @@ data = np.array(data) # I guess the difference between list and np array is the
                       # speed numpy brings.
 
 # extract some data to get to know the dataset
-gender = []
+numMales = 0
+numFemales = 0
 age = []
+
 for row in data :
-  if row[4] not in gender:
-    gender.append(row[4])
+  # Get number of Males and Females respectively from the passenger list
+  if row[4] == 'male':
+    numMales = numMales + 1
+  if row[4] == 'female':
+    numFemales = numFemales + 1
+
+  # Get all ages represented among the passengers
   if row[5] != '':
     if float(row[5]) not in age:
       age.append(float(row[5]))
 
-#age.sort()
-#print age
-print len(age)
-print gender
-print len(data)
+
+# Print the extracted data
+
+print "Ages:"
+print age
+print "Number of different ages: ", len(age)
+print "Number of entries: ", len(data)
+print "Number of females: ", numFemales
+print "Number of males: ", numMales
+print "Is all passengers considered Male of Female? : ", (numFemales + numMales) == len(data) 
 #print data[13::,9] # This prints column 9 from each row, starting at row 13.
