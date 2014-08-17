7fa07816bddf5ae66c75b350a4d21e286e9d0ba4
Freddeb <f.bredmar@gmail.com>
2014-08-18 00:55:36 +0200

    Added some extraction for survivors and non-surviviors. Gonna try to mash some extractions together now, to eliminate some if-else (hopefully)

--- visualization.py
+++ visualization.py
@@ -2,6 +2,13 @@
 
 import csv as csv
 import numpy as np
+
+#----------------------------------------------#
+# Links between the data headers and the index.#
+# Not suited for changes in the data-----------#
+# another option might be better.--------------#
+#----------------------------------------------#
+
 # These are the columns of the csv file.
 # PassengerId,Survived,Pclass,Name,Sex,Age,SibSp,Parch,Ticket,Fare,Cabin,Embarked
 csv_file = csv.reader(open('dataset/train.csv', 'rb'))
@@ -15,17 +22,32 @@ for row in csv_file:
 data = np.array(data) # I guess the difference between list and np array is the
                       # speed numpy brings.
 
-# extract some data to get to know the dataset
+# Extract some data to get to know the dataset.
+
+#Declare variables storing the data we want.
 numMales = 0
 numFemales = 0
 age = []
+numWomenDead = 0
+numWomenSurvive = 0
+numMenDead = 0
+numMenSurvive = 0
+numSurvive = 0
+numDead = 0
 
+# Single-loop for data extraction.
 for row in data :
   # Get number of Males and Females respectively from the passenger list
   if row[4] == 'male':
     numMales = numMales + 1
   if row[4] == 'female':
     numFemales = numFemales + 1
+
+  # Get number of survivers / non-survivers
+  if row[1] == "0":
+    numDead = numDead + 1
+  if row[1] == "1":  # Could have been an else-statement. isn't due to clarity ATM.
+    numSurvive = numSurvive + 1
 
   # Get all ages represented among the passengers
   if row[5] != '':
@@ -35,11 +57,13 @@ for row in data :
 
 # Print the extracted data
 
-print "Ages:"
-print age
+print header
+print "Survivors: ", numSurvive
+print "Casulties: ", numDead
+print "Ages:", age
 print "Number of different ages: ", len(age)
 print "Number of entries: ", len(data)
 print "Number of females: ", numFemales
 print "Number of males: ", numMales
-print "Is all passengers considered Male of Female? : ", (numFemales + numMales) == len(data) 
+print "Is all passengers considered Male of Female? : ", (numFemales + numMales) == len(data)
 #print data[13::,9] # This prints column 9 from each row, starting at row 13.
